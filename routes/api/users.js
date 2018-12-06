6c5e505bc8a11388bf28a737e61f4ac6b32a18aa
krishnanath <isabella0484@gmail.com>
2018-12-06 19:57:12 +0530

    PostReq

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,4 +9,10 @@ router.get('/test', (req, res) =>res.json({
     msg: "Users Works"
 }));
 
+
+
+router.post('/register', (req, res)=> {
+    User.findOne({email: req.body.email})
+});
+
 module.exports = router;
