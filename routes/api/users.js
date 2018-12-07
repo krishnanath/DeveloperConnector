4c4eb8a1a85f9364336a03b899e4b221348e8869
krishnanath <isabella0484@gmail.com>
2018-12-07 20:49:33 +0530

    GETapi

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,3 +16,23 @@ router.post('/register', (req, res)=> {
 });
 
 module.exports = router;
+
+
+
+//fordailyact
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+      });
+    }
+  );
+  
+  module.exports = router;
