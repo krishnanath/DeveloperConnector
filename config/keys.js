fb8af51639557b6a6c8f1d0699079f73c9f27d26
krishnanath <isabella0484@gmail.com>
2018-12-15 19:13:40 +0530

    DeprecationWarning

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports ={
-    mongoURI:'mongodb://anjitha1111:anjitha1111@ds157843.mlab.com:57843/customer_api',
+    mongoURI:'mongodb://anjitha1111:anjitha1111@ds157843.mlab.com:57843/customer_api', 
+ 
     secertOrKey:'secret'
 }
--- config/passport.js
+++ config/passport.js
@@ -2,15 +2,25 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("../config/keys");
+const keys = require("../config/keys"); 
 
-const opt = {};
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secertOrKey;
 
 module.exports = passport => {
     passport.use(
         new JwtStrategy(opts, (jwt_paload, done) => {
-        console.log(jwt_payload);
+     User.findById(jwt_paload.id)
+     .then(user => {
+         if(user){
+             return done(null, user);
+
+
+         }
+         return done(null, false);
+
+     })
+     .catch(err => console.log(err));
     }));
 };
